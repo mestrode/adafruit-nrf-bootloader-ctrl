7056a70cfcea7f857ecb2312a1700fd4c19ce190
mestrode <35630715+mestrode@users.noreply.github.com>
2026-02-26 19:49:36 +0100

    doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,13 @@
-//! Bootloader modules for nrf52 boards
-//! Provides functions to reset the decive into different bootloader modes
+//! Controller for Adafruit nRF52 Bootloader
+//! 
+//! Provides functions to reset the device into different bootloader modes.
+//! This is useful for example to reset into DFU mode to update the firmware.
+//! 
+//! Adafruit nRF52 Bootloader repo: <https://github.com/adafruit/Adafruit_nRF52_Bootloader>
+//! 
+//! How to update / flash the bootloader onto your board:
+//! <https://learn.adafruit.com/introducing-the-adafruit-nrf52840-feather/update-bootloader>
 
 #![no_std]
 
 pub mod reset;
-
-/// adafruit nrf52 bootloader controller
-/// 
-/// This module provides functions to reset the device into different bootloader modes.
-/// This is useful for example to reset into DFU mode to update the firmware.
-/// 
-/// Adadrfuit bootloader repo: <https://github.com/adafruit/Adafruit_nRF52_Bootloader>
-/// 
-/// Bootloader Update: <https://learn.adafruit.com/introducing-the-adafruit-nrf52840-feather/update-bootloader>
-
-pub use reset::*;
